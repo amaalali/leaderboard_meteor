7e886fe63a5f072511b52a20f116196c3504a628
Amaal Ali <amq.ali@gmail.com>
2016-12-06 11:18:48 +0000

    Select user and visualy identify by higlighting. Add points and remove points.

--- leaderboard.js
+++ leaderboard.js
@@ -4,11 +4,27 @@ if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function() {
       return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return "selected";
+      }
     }
   });
   Template.leaderboard.events({
-    'click .player': function() {
-      console.log("You click on a .player!");
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, { $inc: { score: 5 }});
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, { $inc: { score: -5}})
     }
   });
 }
